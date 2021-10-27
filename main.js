150a05177df7afeab0c20fce0328d7ebc96fe202
Loba980 <78515908+Loba980@users.noreply.github.com>
2021-10-27 19:47:40 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 https://teachablemachine.withgoogle.com/models/wZ6TgeAdv/model.json
+
+function start(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/wZ6TgeAdv/model.json", modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(){
+    if(error){
+        console.error(error);
+}
+
+else{
+    console.log(gotResults);
+}
+}
