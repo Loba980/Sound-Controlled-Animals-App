257cc7e4518e30434d775f221c2e32f2e03dc907
Loba980 <78515908+Loba980@users.noreply.github.com>
2021-10-27 20:20:33 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-https://teachablemachine.withgoogle.com/models/wZ6TgeAdv/model.json
+dog=0;
+cat=0;
+
 
 function start(){
     navigator.mediaDevices.getUserMedia({audio:true});
@@ -16,5 +18,22 @@ function gotResults(){
 
 else{
     console.log(gotResults);
+    var red=Math.floor(Math.random()*255) + 1;
+    var blue=Math.floor(Math.random()*255) + 1; 
+    var green=Math.floor(Math.random()*255) + 1;
+
+    document.getElementById("number_of_sounds_heard").style.red;
+    results=document.getElementById("sound").style.green;
+    image="img";
+
+    if("sound"==barking){
+image.src="dog_image.jpg";
+dog=1;
 }
+
+    else("sound"==meowing);{
+image.src="cat_image.jpg";
+cat=1;
+    }
+ }
 }
